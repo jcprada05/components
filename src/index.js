5a47d7a200ed129decc17de060d565cfcf29c563
jcprada05 <jcprada05@gmail.com>
2022-07-08 18:33:45 -0500

    Approval Card, Children Props

--- src/index.js
+++ src/index.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import { faker } from '@faker-js/faker';
+import './style/App.css';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        avatar={faker.image.avatar()}
-        content="Nice blog Post 1!"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        avatar={faker.image.avatar()}
-        content="Nice blog Post 2!"
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00PM"
-        avatar={faker.image.avatar()}
-        content="Nice blog Post 3!"
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          avatar={faker.image.avatar()}
+          content="Nice blog Post 1!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          avatar={faker.image.avatar()}
+          content="Nice blog Post 2!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          avatar={faker.image.avatar()}
+          content="Nice blog Post 3!"
+        />
+      </ApprovalCard>
     </div>
   );
 };
