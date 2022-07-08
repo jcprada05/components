1c2db4aab4c651d78ec0b3dedbdb0b498d6fa62b
jcprada05 <jcprada05@gmail.com>
2022-07-08 10:11:31 -0500

    First commit - Parent and child components, faker, use props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import { faker } from '@faker-js/faker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        avatar={faker.image.avatar()}
+        content="Nice blog Post 1!"
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        avatar={faker.image.avatar()}
+        content="Nice blog Post 2!"
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        avatar={faker.image.avatar()}
+        content="Nice blog Post 3!"
+      />
+    </div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.querySelector('#root'));
